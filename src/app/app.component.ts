a531a773ae0171934a5be1f9e2eea77489341ee2
Regnaroz <k-0-1@outlook.com>
2022-01-08 19:15:11 +0200

    list for Versions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -10,34 +11,49 @@ import { FormsModule } from '@angular/forms';
 })
 export class AppComponent {
   title = 'uploadFile';
-  apiUrl = 'https://localhost:44349/api/XApi/UploadScorm';
-  isValid=false
-  constructor(private http: HttpClient) {}
-  
+ 
+  isValid = false;
+  versionList: any;
+  versionId=1;
+  apiUrl = 'https://localhost:44339/api/FilesData/UplodZIP/';
+  constructor(private http: HttpClient) {
+    http
+      .get('https://localhost:44339/api/ScormVersion/GetScormVersion')
+      .subscribe(
+        (data) => {
+          this.versionList = data;
+          
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
 
-checkFile(file:any){
-  if(file.length===0){
-   this.isValid=false
   }
-  console.log(true);
-  
-  this.isValid= true
-}
+
+  checkFile(file: any) {
+    if (file.length === 0) {
+      this.isValid = false;
+    }
+    console.log(true);
+
+    this.isValid = true;
+  }
 
   uploadFile(files: any) {
     if (files.length === 0) {
       return;
     }
-    console.log(files);
 
     let fileToUpload = <File>files[0];
     const formData = new FormData();
     console.log(fileToUpload);
     formData.append('file', fileToUpload, fileToUpload.name);
+
     this.uploadAttachment(formData);
   }
 
-  uploadAttachment(file: FormData) {
+  uploadAttachment(file: any) {
     const headerDict = {
       'Content-Type': 'application/json',
       Accept: 'application/json',
@@ -45,14 +61,20 @@ checkFile(file:any){
     const requestOptions = {
       headers: new HttpHeaders(headerDict),
     };
-    this.http.post(this.apiUrl, file).subscribe(
+
+    this.http.post(this.apiUrl.concat(this.versionId.toString()), file).subscribe(
       (data: any) => {
         if (data) {
           console.log(data);
         }
       },
-      (err) => {console.log(err);
+      (err) => {
+        console.log(err);
       }
     );
   }
+
+  selectVersion(id:any){
+    this.versionId=id
+   }
 }
